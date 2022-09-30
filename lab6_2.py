5c6c5114e8a2018ff71935dfc916496f01d19461
Dormant512 <mikegrig@inbox.ru>
2022-09-30 18:56:20 +0300

    lab6_2 done

--- lab6_2.py
+++ lab6_2.py
@@ -4,35 +4,29 @@ import matplotlib.pyplot as plt
 import random
 import time
 
-vert_num = 100
-edge_num = 500
-max_weight = 20
+rows = 10
+cols = 20
+obstacle_num = 40
 
-#random adjacency generation
-G_adj_matrix = np.zeros((vert_num, vert_num))
-edge_list = [x for x in range(sum(range(vert_num)))]
-edge_exist_indices = random.sample(edge_list, edge_num)
-edge_binary_flat = []
-for i in edge_list:
-    if i in edge_exist_indices:
-        edge_binary_flat.append(1)
-    else:
-        edge_binary_flat.append(0)
+indices_flat = list(range(rows*cols))
+grid_flat = np.zeros((rows*cols,))
+obstacles = random.sample(indices_flat, obstacle_num)
+for i in obstacles:
+    grid_flat[i] = 1
+grid = grid_flat.reshape(rows, cols)
+print(grid)
 
 #building a graph in networkx
 G = nx.Graph()
-G.add_nodes_from(range(vert_num))
+for i in range(rows):
+    for j in range(cols):
+        if grid[i][j] != 1:
+            G.add_node(f"({i},{j})", pos=(i,j))
+            if (i + 1) < rows and grid[i+1][j] != 1:
+                G.add_edge(f"({i+1},{j})", f"({i},{j})")
+            if (j + 1) < cols and grid[i][j+1] != 1:
+                G.add_edge(f"({i},{j+1})", f"({i},{j})")
 
-#building a symmetrical adjacency matrix
-k = 0 #index for existing edges
-for i in range(vert_num):
-    for j in range(i+1, vert_num):
-        if edge_binary_flat[k] == 1:
-            ans = np.random.randint(max_weight) + 1
-            G_adj_matrix[i][j] = ans
-            G_adj_matrix[j][i] = ans
-            G.add_edge(i, j, weight=ans)
-        k += 1
 
 def path_vert2edge(path):
     edge_path = []
@@ -42,39 +36,15 @@ def path_vert2edge(path):
 
 
 if __name__ == "__main__":
-    print(G_adj_matrix[0:3])
+    pos = nx.get_node_attributes(G, 'pos')
+    nx.draw(G, pos, node_shape='s', node_size=150, node_color="skyblue", with_labels=False)
+    random_nodes = list(random.sample(list(G.nodes), 2))
+    nx.draw_networkx_nodes(G, pos, node_shape='s', node_size=150, node_color='orange', nodelist=random_nodes)
+    
+    short_p_nodes = nx.shortest_path(G, source=random_nodes[0], target=random_nodes[1])
+    short_p_edges = path_vert2edge(short_p_nodes)
 
-    src, trg = np.random.randint(vert_num), np.random.randint(vert_num) #random start and end nodes
-
-    pos = nx.circular_layout(G)
-    nx.draw_networkx(G, pos, font_weight='bold', node_color='skyblue', edge_color='steelblue')
-    nx.draw_networkx_nodes(G, pos, nodelist=[src, trg], node_color='tomato')
-    labels = nx.get_edge_attributes(G, 'weight')
-
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-
-    dijkstra_times = []
-    for i in range(10):
-        start_time = time.time()
-        dijkstra_p = nx.shortest_path(G, source=src, target=trg, weight='weight', method='dijkstra')
-        delta_time = time.time() - start_time
-        dijkstra_times.append(delta_time)
-
-    bellman_ford_times = []
-    for i in range(10):
-        start_time = time.time()
-        bellman_ford_p = nx.shortest_path(G, source=src, target=trg, weight='weight', method='bellman-ford')
-        delta_time = time.time() - start_time
-        bellman_ford_times.append(delta_time)
-        
-    print(f"Source: {src}    Target: {trg}")
-    print(f"Dijkstra: {dijkstra_p}    Time: {np.mean(dijkstra_times)}s")
-    print(f"Bellman-Ford: {bellman_ford_p}    Time: {np.mean(bellman_ford_times)}s")
-
-    path_edges = path_vert2edge(dijkstra_p)
-    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='firebrick', width=1.5)
-
-    costs = [int(G[e[0]][e[1]]['weight']) for e in path_edges]
-    print(f"Path costs: {costs}    Sum: {sum(costs)}")
+    nx.draw_networkx_nodes(G, pos, node_shape='s', node_size=150, node_color='sandybrown', nodelist=short_p_nodes)
+    nx.draw_networkx_edges(G, pos, edgelist=short_p_edges, edge_color='peru')
 
     plt.show()
