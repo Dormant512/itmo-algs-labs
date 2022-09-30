b71ca5c432ad7f57e037e0787d6e9b54bc0084ef
Dormant512 <mikegrig@inbox.ru>
2022-09-30 20:50:25 +0300

    lab6 full done

--- lab6_2.py
+++ lab6_2.py
@@ -37,14 +37,25 @@ def path_vert2edge(path):
 
 if __name__ == "__main__":
     pos = nx.get_node_attributes(G, 'pos')
-    nx.draw(G, pos, node_shape='s', node_size=150, node_color="skyblue", with_labels=False)
-    random_nodes = list(random.sample(list(G.nodes), 2))
-    nx.draw_networkx_nodes(G, pos, node_shape='s', node_size=150, node_color='orange', nodelist=random_nodes)
-    
-    short_p_nodes = nx.shortest_path(G, source=random_nodes[0], target=random_nodes[1])
-    short_p_edges = path_vert2edge(short_p_nodes)
 
-    nx.draw_networkx_nodes(G, pos, node_shape='s', node_size=150, node_color='sandybrown', nodelist=short_p_nodes)
-    nx.draw_networkx_edges(G, pos, edgelist=short_p_edges, edge_color='peru')
+    num_of_runs = 5
+    nd_size = 100
+    nd_shape = 's'
+    for i in range(num_of_runs):
+        plt.subplot(1, num_of_runs, i+1)
+        nx.draw(G, pos, node_shape=nd_shape, node_size=nd_size, node_color="skyblue", with_labels=False)
+        random_nodes = list(random.sample(list(G.nodes), 2))
+    
+        start_time = time.time()
+        short_p_nodes = nx.shortest_path(G, source=random_nodes[0], target=random_nodes[1])
+        time_taken = time.time() - start_time
+        time_title = "{:.4f}".format(time_taken * 1000)
+        
+        short_p_edges = path_vert2edge(short_p_nodes)
+        plt.title(f"From {random_nodes[0]} to {random_nodes[1]} \n{time_title}ms")
+
+        nx.draw_networkx_nodes(G, pos, node_shape=nd_shape, node_size=nd_size, node_color='orange', nodelist=short_p_nodes)
+        nx.draw_networkx_edges(G, pos, edgelist=short_p_edges, edge_color='peru')
+        nx.draw_networkx_nodes(G, pos, node_shape=nd_shape, node_size=nd_size, node_color='darkorange', nodelist=random_nodes)
 
     plt.show()
