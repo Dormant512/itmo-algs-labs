4102941993dd1496ea61f938d41026a07d27a474
SemenovaValeria <84026566+SemenovaValeria@users.noreply.github.com>
2022-10-02 23:03:46 +0500

    done

--- lab5.py
+++ lab5.py
@@ -2,7 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from collections import deque
+import trimesh
 
 vert_num = 100
 edge_num = 200
@@ -52,24 +52,6 @@ def adj_matrix2list(adj_matrix, is_dict=False):
             adj_list.append(neighbors)
     return adj_list
 
-
-def connected_components(graph):
-    """Function that finds connected components"""
-    done = set()
-    for i in range(len(graph)):
-        if i not in done:
-            done.add(i)
-            component = []
-            que = deque([i])
-            while que:
-                node = que.popleft()
-                component.append(node)
-                for i in graph[node]:
-                    if i not in done:
-                        done.add(i)
-                        que.append(i)
-            yield component
-
 def path_painter(path):
     """Function that is required to draw the shortest path"""
     edge_path = []
@@ -79,18 +61,41 @@ def path_painter(path):
 
 pos = nx.circular_layout(G)
 
+
+visited = [False for i in range(vert_num+1)]
+def dfs(node, component):
+    """Function that searches for graph vertices using the DFS method"""
+    visited[node] = True
+    component.append(node)
+    for neighbour in G[node]:
+        if visited[neighbour] == False:
+            dfs(neighbour,component)
+
+
 if __name__ == "__main__":
     print("\nSeveral rows of the adjacency matrix:\n", G_adj_matrix[0:3])
     a_l = adj_matrix2list(G_adj_matrix)
 
     print("\nAdjacency list:\n",a_l[0:3])
 
-    print("\nConnected components of the graph (standalone vertices have no connections):\n",list(connected_components(G)))
+    #loop searching for connected vertices
+    all_components = []
+    for i in range(vert_num):
+        if visited[i] == False:
+            component = []
+            dfs(i, component)
+            all_components.append(component)   
+
+    print("\nAll connected vertices of the graph (standalone vertices have no connections):\n",list(all_components))
+
+    print("\nAll connscted vertices (in pairs):\n",list(nx.edge_dfs(G)))
 
     #getting random points, calculating the shortest path and its length
         #it is necessary to add 1 to the calculated length due to ignoring the starting point in the calculation
     source,target = np.random.randint(vert_num), np.random.randint(vert_num)
-    path=nx.shortest_path(G, source=source, target=target)
+
+    #according to the documentation, when weight = None, the BFS method is used
+    path=nx.shortest_path(G, source=source, weight = None,target=target) 
     path_len=nx.shortest_path_length(G, source=source, target=target)+1
 
     print("\nShortest path between random point",source, "and random point", target, "consists of", path_len, "points:", path,'\n')
